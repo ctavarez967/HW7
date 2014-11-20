24ec3354cb40dc45db6bead76bb5487e63fa247d
Cristian Tavarez <cristian-tavarez@uiowa.edu>
2014-11-20 08:18:59 -0600

    fixed background and clock

--- Blackhawks.master.vb
+++ Blackhawks.master.vb
@@ -1,5 +1,17 @@
 ﻿
 Partial Class Blackhawks
     Inherits System.Web.UI.MasterPage
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        Dim thisDate As Date
+        thisDate = Today
+        lbl_date.Text = thisDate
+
+        Dim currentTime As Date
+        currentTime = TimeOfDay
+        lbl_time.Text = currentTime
+
+    End Sub
 End Class
 
